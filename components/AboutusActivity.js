d7cf6276e90703e6aeac53d86cd2f7c2edb1bf52
gauravjainn <itsgauravjain.gj@gmail.com>
2020-05-18 01:19:03 +0530

    about us screen completed

--- components/AboutusActivity.js
+++ components/AboutusActivity.js
@@ -13,13 +13,29 @@ import {
   Image,
   ScrollView,
   ImageBackground,
-  SafeAreaView
+  SafeAreaView,
+  FlatList
 } from 'react-native';
 import ActionButton from 'react-native-circular-action-menu';
-import AsyncStorage from '@react-native-community/async-storage';
-
 
 var deviceType;
+
+function Item({ item }) {
+  return (
+    <View style={styles.listItem}>
+      <View style={{ alignItems: "center", flexDirection: 'column', width: 100, height: 100 }}>
+
+        <Image source={{ uri: item.photo }} style={{ width: 80, height: 80, borderRadius: 50 , 
+          borderColor: '#0093c8', borderWidth:2}} />
+
+        <View style={{ alignItems: "center", flexDirection: 'column' }}>
+          <Text style={{ fontWeight: "bold", fontSize:12 }}>{item.name}</Text>
+        </View>
+
+      </View>
+    </View>
+  );
+}
 
 
 class AboutusActivity extends Component {
@@ -32,7 +48,69 @@ class AboutusActivity extends Component {
       password: '',
       status: '',
       wholeResult: '',
-      baseUrl: 'http://203.190.153.22/yys/admin/app_api/customer_login'
+      baseUrl: 'http://203.190.153.22/yys/admin/app_api/customer_login',
+      data: [
+        {
+          "name": "Miyah Myles",
+          "email": "miyah.myles@gmail.com",
+          "position": "Data Entry Clerk",
+          "photo": "https:\/\/images.unsplash.com\/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6"
+        },
+        {
+          "name": "June Cha",
+          "email": "june.cha@gmail.com",
+          "position": "Sales Manager",
+          "photo": "https:\/\/randomuser.me\/api\/portraits\/women\/44.jpg"
+        },
+        {
+          "name": "Iida Niskanen",
+          "email": "iida.niskanen@gmail.com",
+          "position": "Sales Manager",
+          "photo": "https:\/\/randomuser.me\/api\/portraits\/women\/68.jpg"
+        },
+        {
+          "name": "Renee Sims",
+          "email": "renee.sims@gmail.com",
+          "position": "Medical Assistant",
+          "photo": "https:\/\/randomuser.me\/api\/portraits\/women\/65.jpg"
+        },
+        {
+          "name": "Jonathan Nu\u00f1ez",
+          "email": "jonathan.nu\u00f1ez@gmail.com",
+          "position": "Clerical",
+          "photo": "https:\/\/randomuser.me\/api\/portraits\/men\/43.jpg"
+        },
+        {
+          "name": "Sasha Ho",
+          "email": "sasha.ho@gmail.com",
+          "position": "Administrative Assistant",
+          "photo": "https:\/\/images.pexels.com\/photos\/415829\/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb"
+        },
+        {
+          "name": "Abdullah Hadley",
+          "email": "abdullah.hadley@gmail.com",
+          "position": "Marketing",
+          "photo": "https:\/\/images.unsplash.com\/photo-1507003211169-0a1dd7228f2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=a72ca28288878f8404a795f39642a46f"
+        },
+        {
+          "name": "Thomas Stock",
+          "email": "thomas.stock@gmail.com",
+          "position": "Product Designer",
+          "photo": "https:\/\/tinyfac.es\/data\/avatars\/B0298C36-9751-48EF-BE15-80FB9CD11143-500w.jpeg"
+        },
+        {
+          "name": "Veeti Seppanen",
+          "email": "veeti.seppanen@gmail.com",
+          "position": "Product Designer",
+          "photo": "https:\/\/randomuser.me\/api\/portraits\/men\/97.jpg"
+        },
+        {
+          "name": "Bonnie Riley",
+          "email": "bonnie.riley@gmail.com",
+          "position": "Marketing",
+          "photo": "https:\/\/randomuser.me\/api\/portraits\/women\/26.jpg"
+        }
+      ]
     };
   }
 
@@ -118,12 +196,14 @@ class AboutusActivity extends Component {
 
       <SafeAreaView style={styles.container}>
 
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', 
-        backgroundColor: '#ffffff', height: 60 }}>
+        <View style={{
+          flexDirection: 'row', alignItems: 'center', justifyContent: 'center',
+          backgroundColor: '#ffffff', height: 60
+        }}>
 
           <TouchableOpacity style={{ flex: .20, alignItems: 'center', justifyContent: 'center' }}
-             onPress={() => { this.props.navigation.goBack()}} >
-            
+            onPress={() => { this.props.navigation.goBack() }} >
+
 
             <Image
               source={require('../images/back_blue.png')}
@@ -151,58 +231,63 @@ class AboutusActivity extends Component {
           </TouchableOpacity>
         </View>
 
-        <ScrollView style={{ flexDirection: 'column'}} >
+
+        <View style={{ flexDirection: 'column', marginTop: 20, flex: 1 }}>
+
+
 
           <View style={{
-            flexDirection: 'row', backgroundColor: '#ffffff', borderBottomRightRadius: 20, 
-            marginBottom:20,
-           borderBottomLeftRadius: 20, height: 200, width:392,  alignItems: 'center', elevation:20,
-           shadowColor: '#ecf6fb' }}>
+            flexDirection: 'row', backgroundColor: '#ffffff', borderBottomRightRadius: 20,
+            marginBottom: 20, flex: .4,
+            borderBottomLeftRadius: 20, width: 392, elevation: 20,
+            shadowColor: '#ecf6fb'
+          }}>
+
+            <ScrollView style={{ flexDirection: 'column', nestedScrollEnabled: true }} >
+
+              <Text style={{ color: '#4d4d4d', marginLeft: 10, marginRight: 10 }}>Rahul Kumaksdhskjhdkjshdkjshkdhkshdkjshdkjshkjshdksh
+              dkjhsdshdkjshdkjshdkshjkdhsdsjhdkjshdkjshkjshdkshkjshdshdhsdhjkshdkjshdkjshkjdhskdhskjhd
+              kjshdkjshkdhskjdjshdksjhdjkshdjkhskdhskjdhksjhdkshdkjshkjdhskjhdkjshkjshjshdjkshdkjshdksj\
+              dsjdhskjhdshdkshkshkdhskdhshdskjhkjshkshdkshdksjhdkshkdhskdhskjhdkjshdkshdkshdskjhdksdhskr
+              Rahul Kumaksdhskjhdskjdjshdksjhdjkshdjkhskdhskjdhksjhdkshdkjshkjdhskjhdkjshkjshjshdjkshdkjshdksj\
+              dsjdhskjhdshdkshkshkdhskdhshdskjhkjshkshdkshdksjhdkshkdhskdhskjhdkjshdkshdkshdskjhdksdhskr
+              Rahul Kumaksdhskjhdkjshdkjshkdhkshdkjshdkjshkjshdkshkjshdkjshkdhkshdkjshdkjshkjshdksh
+              dkjhsdshdkjshdkjshdkshjkdhsdsjhdkjshdkjshkjshdkshkjshdshdhsdhjkshdkjshdkjshkjdhskdhskjhd
+              kjshdkjshkdhskjdjshdksjhdjkshdjkhskdhskjdhksjhdkshdkjshkjdhskjhdkjshkjshjshdjkshdkjshdksj\
+            dsjdhskjhdshdkshkshkdhskdhshdskjhkjshkshdkshdksjhdkshkdhskdhskjhdkjshdkshdkshdskjhdksdhskr</Text>
+
+            </ScrollView>
+
 
           </View>
 
-      
 
+          <View style={{ flexDirection: 'column', marginTop: 20, flex: .6 }}>
 
+            <Text style={{
+              color: "#0093c8",
+              borderBottomColor: "#0093c8",
+              width: 150,
+              fontWeight: 'bold',
+              fontSize: RFPercentage(2), paddingLeft: 10, borderBottomWidth: 2, padding: 5
+            }}>  PORTFOLIO </Text>
 
-          {/* <View style={styles.datacontainer}>
-            <View style={styles.SectionStyle}>
+            <View style={{ borderBottomColor: '#aaaaaa', borderBottomWidth: 1 }} />
 
-              <Image source={require('../images/email.png')}
-                style={styles.ImageIconStyle} />
-
-              <TextInput
-                placeholderTextColor="#C7E8F2"
-                onChangeText={email => this.setState({ email })}
-                placeholder={'Email'}
-                underlineColorAndroid="transparent"
-                style={styles.input}
-              />
-
-            </View>
-
-            <View style={styles.SectionStyle}>
-
-              <Image source={require('../images/lock.png')}
-                style={styles.ImagelockIconStyle} />
-
-
-              <TextInput
-                placeholder={'Password'}
-                placeholderTextColor="#C7E8F2"
-                underlineColorAndroid="transparent"
-                style={styles.input}
-                secureTextEntry={true}
-                onChangeText={password => this.setState({ password })}
+            <View style={styles.list_container}>
+              <FlatList
+                style={{ flex: 1 }}
+                data={this.state.data}
+                renderItem={({ item }) => <Item item={item} />}
+                keyExtractor={item => item.email}
+                numColumns={3}
               />
             </View>
 
+          </View>
+        </View>
 
-          </View> */}
-
-
-
-        </ScrollView>
+        {/* </ScrollView> */}
 
 
         <View style={{
@@ -352,7 +437,21 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'center',
-},
+  },
+  list_container: {
+    flex: 1,
+    backgroundColor: '#F0F5FE'
+  },
+  listItem: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: "#F0F5FE",
+    width: "80%",
+    flex: 1,
+    alignSelf: "center",
+    flexDirection: "row",
+    borderRadius: 5
+  }
 
 });
 
